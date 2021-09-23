c9fa713da23ed6b01a57c9e170cad3181915ea50
RomanZorkin <zorkin.roman@gmail.com>
2021-09-23 19:12:40 +0700

    next update

--- table_admin.py
+++ table_admin.py
@@ -4,10 +4,11 @@ start_time1 = time.time()
 import file_reader 
 
 
-file_reader.Excel_reader('deals').table_to_sql()
-file_reader.Excel_reader('purchases').table_to_sql()
-file_reader.Excel_reader('budget_commitment').table_to_sql()
-file_reader.Excel_reader('payments_full').table_to_sql()
-file_reader.Excel_reader('payments_short').table_to_sql()
+#file_reader.Excel_reader('deals').table_to_sql()
+#file_reader.Excel_reader('purchases').table_to_sql()
+#file_reader.Excel_reader('budget_commitment').table_to_sql()
+#file_reader.Excel_reader('payments_full').table_to_sql()
+#file_reader.Excel_reader('payments_short').table_to_sql()
+file_reader.Excel_reader('commitment_treasury').table_to_sql()
 
 print("Время выполнения table_admin%s" % (time.time() - start_time1))
