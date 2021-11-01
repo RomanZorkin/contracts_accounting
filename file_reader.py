5155167c294537d859c9eb1bd283717c4475a45b
RomanZorkin <zorkin.roman@gmail.com>
2021-11-01 15:25:51 +0700

    file_reader.py refactoring
    file_reader.py refactoring

--- file_reader.py
+++ file_reader.py
@@ -12,16 +12,15 @@ import yaml
 from sqlorm import cleaner, sql_admin
 
 start_time = time.time()
-print('время выполнения import%s' % (time.time() - start_time))
-
 config_file = Path('configuration/table.yaml')
 
 with open(config_file) as fh:
     table_config = yaml.load(fh, Loader=yaml.FullLoader)
 
 logging.debug(
-    "Scheme class inicialisation %s" % (time.time() - start_time),
+    'Scheme class inicialisation %s' % (time.time() - start_time),
 )
+
 
 class Reader(object):
     """Transfers information from a given file to a database.
@@ -35,25 +34,25 @@ class Reader(object):
     verified data is recorded in the database.
 
     Attributes:
-        table_name (str): the name of the table for which we apply the
+        table_name (str): the name of the table for which we apply the\
             corresponding rules from configuration file "table.yaml".
-        table_scheme (Dict): a dictionary with all the rules, parameters and
-            settings for the table. The following parameters are passed:
-            the name of the table, the name of the file to read,
-            the sheet to read, the column settings (names, data format,
+        table_scheme (Dict): a dictionary with all the rules, parameters and\
+            settings for the table. The following parameters are passed:\
+            the name of the table, the name of the file to read,\
+            the sheet to read, the column settings (names, data format,\
             which are used for sql, the range of cells in excel, etc.).
-        columns_scheme (Dict): dictionary with column parameters:
-            key - column name, value - dictionary with parameters
-            (data format, whether it is part of SQL tables (true or false),
+        columns_scheme (Dict): dictionary with column parameters\
+            key - column name, value - dictionary with parameters\
+            (data format, whether it is part of SQL tables (true or false),\
             whether it is part of excel tables and a range of cells).
-        columns (List): list of columns names.
+        columns (List): list of columns names
         id_sql (str): name of PRIMARY KEY sql column.
-        sql_scheme: sql_admin.Scheme object. Required for
+        sql_scheme: sql_admin.Scheme object. Required for\
             interaction with postgresql.
     """
 
     def __init__(self, table: str):
-        """Inits Reader class.
+        """Init Reader class.
 
         Args:
             table (str): the name of the table for which we apply the
@@ -61,43 +60,137 @@ class Reader(object):
         """
         self.table_name: str = table
         self.table_scheme: Dict[str, Any] = table_config[table]
-        self.columns_scheme: Dict[str, Dict[str, Any]] = self.table_scheme['columns']
+        self.columns_scheme: Dict[
+            str,
+            Dict[str, Any],
+        ] = self.table_scheme['columns']
         self.columns: List[str] = list(self.columns_scheme)
         self.id_sql: str = self._sql_id()
         self.sql_scheme = sql_admin.Scheme(self.table_name)
 
     def _sql_id(self) -> str:
-        """Form name of PRIMARY KEY sql column."""
+        """Form name of PRIMARY KEY sql column.
+
+        Returns:
+            str: PRIMARY KEY sql column name.
+        """
         for column in self.columns:
-            format = self.columns_scheme[column]['format'].split(' ')
-            if len(format) > 1:
+            formating = self.columns_scheme[column]['format'].split(' ')
+            if len(formating) > 1:
                 return column
 
 
-class Excel_reader(Reader):
-    """Reader subclass for reading excel files.
+class FrameHandler(Reader):
+    """Reader subclas for work with pandas DataFrame.
+
+    The class processes the DataFRAME, controls the correctness of
+    the data contained in them, and corrects them if necessary
+    """
+
+    def dict_to_frame(self) -> pd.DataFrame:
+        """Convert dictionary to pandas DataFrame.
+
+        The method generates a dictionary in the format: key - column name;
+        value - a list with column values. The dictionary is converted into
+        a pandas date frame.
+
+        Returns:
+            pd.DataFrame: date frame with data received from the file.
+        """
+        # отловить ошибку формирует словарь в  независимости от знчений redaer
+        # формирует, даже если пусто
+        df_dict = {
+            column: self.read(column) for column in self.excel_columns
+        }
+        logging.debug('finish dict_to_frame %s' % (time.time() - start_time))
+        return pd.DataFrame(df_dict)
+
+    def frame_corrector(self, frame: pd.DataFrame) -> pd.DataFrame:
+        """Format dataframe data.
+
+        For each table in the configuration file, the rules for forming
+        columns are set depending on the data type (int, float, date ...).
+        Each column is checked for compliance with the established rules.
+        If necessary, the data is adjusted.
+
+        Args:
+            frame (pd.DataFrame): DataFrame for control.
+
+        Returns:
+            pd.DataFrame: DataFrame with formatted data.
+        """
+        return cleaner.StructureCleaner(
+            table_name=self.table_name,
+            inframe=frame,
+        ).structure_corrector()
+
+    def frame_to_dict(self, frame: pd.DataFrame) -> Dict[Any, Any]:
+        """Convert pandas DataFrame to dictionary.
+
+        The method converts the pandaы DataFrame into a dictionary.
+        The DataFrame is checked for the correctness of the data,
+        if necessary, the data is formatted. The DataFrame is converted
+        to a dictionary with values line by line - one entry in the
+        dictionary is the values of all columns of the DataFrame on one
+        line. The key in the dictionary is int from 0 to the last line in
+        DataFrame. The table name is added to the dictionary values.
+        The dictionary is checked line by line for compliance with the data
+        available in it, the formats set for this table. The checked strings
+        are overwritten into the primary dictionary.
+
+        Args:
+            frame (pd.DataFrame): DataFrame with file data.
+
+        Returns:
+            Dict[Any, Any]: dict format
+                {0:{'table_name':'deals', 'reg_number':1, 'KBK':"'22'", ... },
+                1:{'table_name':'deals', 'reg_number': 1, 'KBK':"'22'", ... }}
+        """
+        # the "index" value forms the dictionary line by line,
+        # if we take "dict", then the key will be the columns
+        frame_dict = self.frame_corrector(
+            frame,
+        ).to_dict('index')
+        for key, record in frame_dict.items():
+            clean_dict = cleaner.PandasToDict(
+                self.table_name,
+                {
+                    **{'table_name': self.table_name},
+                    **record,
+                },
+            ).clean_rows()
+            frame_dict[key] = clean_dict
+        return frame_dict
+
+
+class ExcelReader(FrameHandler):
+    """FrameHandler subclass for reading excel files.
 
     Read information from excel files and writes it
-    to the postgresql database
+    to the postgresql database. The read data is transferred to the Dataframe.
+    It is more convenient to work with data in this form. The Dataframe is
+    checked for compliance with the established formats and correctness of
+    the received data. After checking and converting the data, they are
+    transferred to the database for writing.
 
     Attributes:
-        excel_file (Path): path to excel file. The path is determined by
-            the configuration file "table.yaml". The internal
+        excel_file (Path): path to excel file. The path is determined by\
+            the configuration file "table.yaml". The internal\
             directory is used - "external_data_source".
-        work_sheet (str): name excel workshit, where the
+        work_sheet (str): name excel workshit, where the\
             information is located.
-        excel_rows (List[str]): the range of rows where
+        excel_rows (List[str]): the range of rows where\
             the information is placed.
         excel_columns (List[str]): excel columns names.
-        excel_cells (Dict[str, List[str]]): dictionary containing: key -
-            the name of the excel column; value - the range of the
+        excel_cells (Dict[str, List[str]]): dictionary containing: key -\
+            the name of the excel column; value - the range of the\
             column to read in the form of a list ['a1', 'a10'].
         wb_obj (openpyxl): openpyxl object - excel workbook (excel file).
         ws (openpyxl): openpyxl object - excel worksheet in workbook.
     """
 
     def __init__(self, table: str):
-        """Inits Reader class.
+        """Init Reader class.
 
         Args:
             table (str): the name of the table for which we apply the
@@ -105,27 +198,72 @@ class Excel_reader(Reader):
         """
         super().__init__(table)
         self.excel_file: Path = Path(self.table_scheme['excel_file'])
-        self.work_sheet: str = self.table_scheme['work_sheet']
+        self._work_sheet: str = self.table_scheme['work_sheet']
         self.excel_rows: List[str] = self.table_scheme['excel_rows']
         self.excel_columns: List[str] = self._excel_columns()
         self.excel_cells: Dict[str, List[str]] = self._excel_cells()
         logging.debug(
-            "start excel inicialisation %s" % (time.time() - start_time),
+            'start excel inicialisation %s' % (time.time() - start_time),
             self.excel_file,
         )
         self.wb_obj = openpyxl.load_workbook(self.excel_file)
-        self.ws = self.wb_obj[self.work_sheet]
-        print("class inicialisation %s" % (time.time() - start_time))
+        self.ws = self.wb_obj[self._work_sheet]
 
-    def _excel_columns(self) -> List[str]:
-        """Form list with columns names according to the configuration file.
+    def read(self, column: str) -> List[Any]:
+        """Generate a list of values by column.
+
+        The method reads the values of the column cells and passes them
+        to the list. The list is processed using the Cleaner module to
+        correct the values and bring them to the specified formats.
+
+        Args:
+            column (str): name of the column with extracted values.
 
         Returns:
-            List[str]: list of columns names
+            List[Any]: list of column values.
         """
-        return [
-            c for c in self.columns if self.columns_scheme[c]['excel'][0]
-        ]
+        clean_list = cleaner.ValueCleaner(
+            [
+                cell[0].value for cell in self.ws[
+                    self.excel_cells[column][0]: self.excel_cells[column][1]
+                ]
+            ],
+            self.table_name,
+            column,
+        )
+        return clean_list.columns_cleaner()
+
+    def list_to_frame(self) -> pd.DataFrame:
+        """Convert data received from a file into a Dataframe.
+
+        The method receives data from each column in turn in the form
+        of a list. The lists are written to the Dataframe.
+
+        Returns:
+            pd.DataFrame: DataFrame with formatted data.
+        """
+        frame = pd.DataFrame(columns=self.excel_columns)
+        for column in self.excel_columns:
+            clean_frame = cleaner.PandasFormat(
+                self.table_name,
+                pd.DataFrame(self.read(column), columns=[column]),
+            ).format_corrector()
+            frame[column] = clean_frame[column]
+        logging.debug('finish list_to_frame %s' % (time.time() - start_time))
+        return frame
+
+    def big_dict_to_sql(self, dict_to_sql: Dict[Any, Any]) -> None:
+        """Start the procedure of writing to the database.
+
+        Args:
+            dict_to_sql (Dict[Any, Any]): dictionary with data to write
+                to the database
+        """
+        self.sql_scheme.insert_big_data(dict_to_sql, self.id_sql)
+
+    def table_to_sql(self) -> None:
+        """Run a method to write data to the database."""
+        self.big_dict_to_sql(self.frame_to_dict(self.list_to_frame()))
 
     def _excel_cells(self) -> Dict[str, List[str]]:
         """Return the range of readable cells in the column.
@@ -136,98 +274,17 @@ class Excel_reader(Reader):
             column to read in the form of a list ['a1', 'a10'].
         """
         return {
-            c: self.columns_scheme[c]['excel'][1] for c in self.excel_columns
+            column: self.columns_scheme[
+                column
+            ]['excel'][1] for column in self.excel_columns
         }
 
-    def read(self, column: str) -> List[Any]:
-        """Generate a list of values by column.
-
-        The function reads the values of the column cells and passes them
-        to the list. The list is processed using the Cleaner module to
-        correct the values and bring them to the specified formats.
-
-        Args:
-            column (str): name of the column with extracted values.
-        Returns:
-            List[Any]: list of column values.
-        """
-        print(f"start read {column} column {(time.time() - start_time)}")
-        clean_list = cleaner.ValueCleaner(
-            [
-                cell[0].value for cell in self.ws[
-                    self.excel_cells[column][0] : self.excel_cells[column][1]
-                ]
-            ],
-            self.table_name,
-            column,
-        )
-        return clean_list.columns_cleaner()
-
-
-    def dict_to_frame(self) -> pd.DataFrame:
-        """Convert dictionary to pandas DataFrame.
-
-
+    def _excel_columns(self) -> List[str]:
+        """Form list with columns names according to the configuration file.
 
         Returns:
-            pd.DataFrame:
+            List[str]: list of columns names
         """
-        # отловить ошибку формирует словарь в  независимости от знчений redaer
-        # формирует, даже если пусто
-        df_dict = {
-            column : self.read(column) for column in self.excel_columns
-        }
-        logging.debug("finish dict_to_frame %s" % (time.time() - start_time))
-        # frame = pd.DataFrame(df_dict)
-        # print(cleaner.PandasCleaner(self.table_name).format_corrector(frame))
-        return pd.DataFrame(df_dict)
-
-    def list_to_frame(self) -> pd.DataFrame:
-        print('start list_to frame')
-        frame = pd.DataFrame(columns=self.excel_columns)
-        for column in self.excel_columns:
-            clean_frame = cleaner.PandasFormat(
-                self.table_name,
-                pd.DataFrame(self.read(column), columns=[column]),
-            ).format_corrector()
-            frame[column] = clean_frame[column]
-        #создает фрейм из колоноко фреймов, которфеы деланм из колоноко ексел и чистим, счала лист, потом, пандас
-        print("finish list_to_frame %s" % (time.time() - start_time))
-        return frame
-
-    def frame_corrector(self, frame: pd.DataFrame) -> pd.DataFrame:
-        return cleaner.StructureCleaner(
-            table_name = self.table_name,
-            inframe = frame
-        ).structure_corrector()
-
-    def frame_to_dict(self)-> Dict:
-        # значение index формирует словарь построчно, если взять dict,
-        # то клю будут колонки
-        frame_dict = self.frame_corrector(
-            self.list_to_frame()
-        ).to_dict('index')
-        for key, value in frame_dict.items():
-            clean_dict = cleaner.PandasToDict(
-                self.table_name,
-                {
-                    **{'table_name' : self.table_name},
-                    **value
-                },
-            ).clean_rows()
-            frame_dict[key] = clean_dict
-        # dict format
-        # {0:{'table_name': 'deals', 'reg_number': 1, 'KBK': "'2223'", ... },
-        #  1:{'table_name': 'deals', 'reg_number': 1, 'KBK': "'2223'", ... }}
-        return frame_dict
-
-    def big_dict_to_sql(self, dict_to_sql) -> None:
-        print("старт запись в базу %s" % (time.time() - start_time))
-        self.sql_scheme.insert_big_data(dict_to_sql, self.id_sql)
-        print("время выполнения %s" % (time.time() - start_time))
-
-    def table_to_sql(self) -> None:
-        self.big_dict_to_sql(self.frame_to_dict())
-
-
-#Excel_reader(table).frame_to_sql()
+        return [
+            col for col in self.columns if self.columns_scheme[col]['excel'][0]
+        ]
